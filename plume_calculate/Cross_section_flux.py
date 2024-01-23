0c9d707c1b7ac04ff83aae3b437d1074003c779a
Liang Menglei <82796664+Emeric53@users.noreply.github.com>
2024-01-23 16:12:49 +0800

    code updates.

---
+++ plume_calculate/Cross_section_flux.py
@@ -0,0 +1,18 @@
+""" This code is used to calculate the cross section flux of the plume.
+by computing the flux through one or more plume cross sections orthogonal to the plume axis.
+"""
+import numpy as np
+from osgeo import gdal
+import math
+
+# Set the path of the plume
+plume_filepath = r"C:\Users\RS\Desktop\EMIT\MethanePlume\EMIT_L2B_CH4PLM_001_20230204T041009_000618_tiff.tif"
+plume_filepath = plume_filepath.replace('\\', '/')
+# Read the plume data and remove the invalid values
+plume_data = gdal.Open(plume_filepath, gdal.GA_ReadOnly)
+plume_data = plume_data.ReadAsArray()
+# Set the pixel resolution, unit: m
+pixel_resolution = 60000
+
+
+
