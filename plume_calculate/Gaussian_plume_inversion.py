a651bb69ac9750a045c25f4ab3f4db17195c028e
Liang Menglei <82796664+Emeric53@users.noreply.github.com>
2024-01-23 16:22:49 +0800

    code updates.

---
+++ plume_calculate/Gaussian_plume_inversion.py
@@ -0,0 +1,24 @@
+"""This code is used to calculate the emission of methane plume by Gaussian plume inversion method"""
+
+import numpy as np
+from osgeo import gdal
+import math
+
+# 设置烟羽的路径
+plume_filepath = r"C:\Users\RS\Desktop\EMIT\MethanePlume\EMIT_L2B_CH4PLM_001_20230204T041009_000618_tiff.tif"
+plume_filepath = plume_filepath.replace('\\', '/')
+
+
+# seriously how to understand the gaussian plume inversion method?
+# how to understand the gaussian plume model?
+
+
+
+
+
+
+
+
+
+
+
