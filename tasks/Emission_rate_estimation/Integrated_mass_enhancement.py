3ca47695cda0a01ee52512f7b895c817e4c43987
Liang Menglei <82796664+Emeric53@users.noreply.github.com>
2024-11-05 20:19:12 +0800

    code updates.

--- tasks/Emission_rate_estimation/Integrated_mass_enhancement.py
+++ tasks/Emission_rate_estimation/Integrated_mass_enhancement.py
@@ -3,7 +3,14 @@ import math
 
 
 # 基于IME算法进行排放量的估算
-def emission_estimate(plume_array, pixel_resolution, windspeed_10m, slope, intercept, enhancement_unit='ppmm'):
+def emission_estimate(
+    plume_array,
+    pixel_resolution,
+    windspeed_10m,
+    slope,
+    intercept,
+    enhancement_unit="ppmm",
+):
     # calculate the area and the length of the plume
     nan_count = np.count_nonzero(~np.isnan(plume_array))
     pixel_area = math.pow(pixel_resolution, 2)
@@ -11,19 +18,24 @@ def emission_estimate(plume_array, pixel_resolution, windspeed_10m, slope, inter
     plume_length = math.sqrt(plume_area)
     # get the values of the plume
     plume_values = [value for value in plume_array.flatten() if value != -9999]
-    if enhancement_unit == 'ppmm':
+    # 转换系数还需再考虑 可能是  1 ppmm 甲烷质量为  7.15 * 10^{-4}
+    if enhancement_unit == "ppmm":
         # convert the unit from  ppm*m to kg/ppm*m, then calculate the integrated mass enhancement
         integrated_mass_enhancement = sum(plume_values) * 0.716 * 0.000001 * pixel_area
-    elif enhancement_unit == 'ppm':
+    elif enhancement_unit == "ppm":
         # convert the unit from  ppm*m to kg/ppm by setting 8km as the scale of troposphere,
         # then calculate the integrated mass enhancement
-        integrated_mass_enhancement = sum(plume_values) * 0.716 * 0.000001 * pixel_area * 8000
+        integrated_mass_enhancement = (
+            sum(plume_values) * 0.716 * 0.000001 * pixel_area * 8000
+        )
     else:
-        print("The unit of the enhancement is not supported, please enter 'ppmm' or 'ppm'.")
+        print(
+            "The unit of the enhancement is not supported, please enter 'ppmm' or 'ppm'."
+        )
     # calculate the effective windspeed with the formula
     effective_windspeed = slope * windspeed_10m + intercept
     # calculate the emission rate of the plume in the unit of kg/h
-    emission_rate = (effective_windspeed * 3600 * integrated_mass_enhancement) / plume_length
+    emission_rate = (
+        effective_windspeed * 3600 * integrated_mass_enhancement
+    ) / plume_length
     return emission_rate
-
-
--- tests/mf_method.py
+++ tests/mf_method.py
@@ -1,1 +1,41 @@
+import math
+import numpy as np
 
+import sys
+
+sys.path.append("../src/")
+
+from utils.satellites_data import general_functions as gf
+
+
+def ml_matched_filter(radiance_cube, unit_absorption_spectrum):
+    band_num, row_num, col_num = radiance_cube.shape
+    methane_enhancement = np.zeros((row_num, col_num))
+
+    background_radiance = np.average(radiance_cube, axis=(1, 2))
+    target_spectrum = background_radiance * unit_absorption_spectrum
+    d_covariance = radiance_cube - background_radiance
+    covariance_matrix = np.zeros((band_num, band_num))
+    for row in range(row_num):
+        for col in range(col_num):
+            covariance_matrix += np.dot(
+                d_covariance[:, row, col], d_covariance[:, row, col]
+            )
+    covariance_matrix /= row_num * col_num
+    covariance_inversed = np.linalg.pinv(covariance_matrix)
+
+    for row in range(row_num):
+        for col in range(col_num):
+            methane_enhancement[row, col] = (
+                (radiance_cube[:, row, col] - background_radiance)
+                @ covariance_inversed
+                @ target_spectrum
+                / (target_spectrum @ covariance_inversed @ target_spectrum)
+            )
+
+    methane_enhancement_mean = np.mean(methane_enhancement)
+    methane_enhancement_std = np.std(methane_enhancement)
+    high_pixel_percentage = np.count_nonzero(methane_enhancement> np.ones_like(methane_enhancement)*(methane_enhancement_mean+methane_enhancement_std)) / (row_num * col_num)
+    if high_pixel_percentage > 0.36 
+        print("High methane enhancement detected!")
+        
